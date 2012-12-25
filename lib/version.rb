9f88c90d456119d84f58e4fcbaa72f696581b81c
Nikita Fedyashev <nfedyashev@gmail.com>
2012-12-26 02:30:38 +0600

    Bump version

--- lib/version.rb
+++ lib/version.rb
@@ -2,7 +2,7 @@ module Retryable
   class Version
     MAJOR = 1 unless defined? Twitter::Version::MAJOR
     MINOR = 3 unless defined? Twitter::Version::MINOR
-    PATCH = 1 unless defined? Twitter::Version::PATCH
+    PATCH = 2 unless defined? Twitter::Version::PATCH
 
     class << self
 
