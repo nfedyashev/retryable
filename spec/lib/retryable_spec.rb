a0445497a383c7551b551d33a3b8b96f0fa83182
Nikita Fedyashev <nfedyashev@gmail.com>
2012-12-26 02:22:30 +0600

    Simplify RSpec expectations

--- spec/lib/retryable_spec.rb
+++ spec/lib/retryable_spec.rb
@@ -6,21 +6,21 @@ describe 'Kernel.retryable' do
     @attempt = 0
   end
 
-  it "should only catch StandardError by default" do
+  it 'catch StandardError only by default' do
     lambda do
       count_retryable(:tries => 2) { |tries, ex| raise Exception if tries < 1 }
     end.should raise_error Exception
     @try_count.should == 1
   end
-  
-  it "should retry on default exception" do
+
+  it 'retries on default exception' do
     Kernel.should_receive(:sleep).once.with(1)
 
     count_retryable(:tries => 2) { |tries, ex| raise StandardError if tries < 1 }
     @try_count.should == 2
   end
 
-  it "should not retry if disabled" do
+  it 'does not retry if disabled' do
     Retryable.disable
 
     lambda do
@@ -29,7 +29,7 @@ describe 'Kernel.retryable' do
     @try_count.should == 1
   end
 
-  it "should execute *ensure* clause" do
+  it 'executes *ensure* clause' do
     ensure_cb  = Proc.new do |retries|
       retries.should == 0
     end
@@ -37,7 +37,7 @@ describe 'Kernel.retryable' do
     Kernel.retryable(:ensure => ensure_cb) { }
   end
 
-  it "should pass retry count and exception on retry" do
+  it 'passes retry count and exception on retry' do
     Kernel.should_receive(:sleep).once.with(1)
 
     count_retryable(:tries => 2) do |tries, ex| 
@@ -46,14 +46,14 @@ describe 'Kernel.retryable' do
     end
     @try_count.should == 2
   end
-  
-  it "should make another try if exception is covered by :on" do
+
+  it 'makes another try if exception is covered by :on' do
     Kernel.stub!(:sleep)
     count_retryable(:on => [StandardError, ArgumentError, RuntimeError] ) { |tries, ex| raise ArgumentError if tries < 1 }
     @try_count.should == 2
   end
 
-  it "should not try on unexpected exception" do
+  it 'does not try on unexpected exception' do
     Kernel.stub!(:sleep)
     lambda do
       count_retryable(:on => RuntimeError ) { |tries, ex| raise StandardError if tries < 1 }
@@ -61,40 +61,40 @@ describe 'Kernel.retryable' do
     @try_count.should == 1
   end
 
-  it "should three times" do
+  it 'retries three times' do
     Kernel.stub!(:sleep)
     count_retryable(:tries => 3) { |tries, ex| raise StandardError if tries < 2 }
     @try_count.should == 3
   end
 
-  it "should retry on default exception" do
+  it 'retries on default exception' do
     Kernel.should_receive(:sleep).once.with(1)
 
     count_retryable(:tries => 2) { |tries, ex| raise StandardError if tries < 1 }
     @try_count.should == 2
   end
 
-  it "exponential backoff scheme for :sleep option" do
+  it 'executes exponential backoff scheme for :sleep option' do
     [1, 4, 16, 64].each { |i| Kernel.should_receive(:sleep).once.ordered.with(i) }
     lambda do
       Kernel.retryable(:tries => 5, :sleep => lambda { |n| 4**n }) { raise RangeError }
     end.should raise_error RangeError
   end
 
-  it "doesn't retry any exception if :on is empty list" do
+  it 'does not retry any exception if :on is empty list' do
     lambda do
       count_retryable(:on => []) { raise }
     end.should raise_error RuntimeError
     @try_count.should == 1
   end
 
-  it "should catch an exception that matches the regex" do
+  it 'catches an exception that matches the regex' do
     Kernel.should_receive(:sleep).once.with(1)
     count_retryable(:matching => /IO timeout/) { |c,e| raise "yo, IO timeout!" if c == 0 }
     @try_count.should == 2
   end
 
-  it "should not catch an exception that doesn't match the regex" do
+  it 'does not catch an exception that does not match the regex' do
     should_not_receive :sleep
     lambda do
       count_retryable(:matching => /TimeError/) { raise "yo, IO timeout!" }
@@ -102,7 +102,7 @@ describe 'Kernel.retryable' do
     @try_count.should == 1
   end
 
-  it "doesn't allow invalid options" do
+  it 'does not allow invalid options' do
     lambda do
       retryable(:bad_option => 2) { raise "this is bad" }
     end.should raise_error ArgumentError, '[Retryable] Invalid options: bad_option'
