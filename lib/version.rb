9d0aa74e900f7ae4f74fdffc19fb8abdf102dee0
Nikita Fedyashev <nfedyashev@gmail.com>
2012-12-26 02:29:34 +0600

    Extract version from gemspec

---
+++ lib/version.rb
@@ -0,0 +1,16 @@
+module Retryable
+  class Version
+    MAJOR = 1 unless defined? Twitter::Version::MAJOR
+    MINOR = 3 unless defined? Twitter::Version::MINOR
+    PATCH = 1 unless defined? Twitter::Version::PATCH
+
+    class << self
+
+      # @return [String]
+      def to_s
+        [MAJOR, MINOR, PATCH].compact.join('.')
+      end
+
+    end
+  end
+end
