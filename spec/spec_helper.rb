0c44b9ab481d9956b21ae9e215c1368a8eccf461
Nikita Fedyashev <nfedyashev@gmail.com>
2012-12-26 02:24:19 +0600

    Extract testing helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-
 require File.dirname(__FILE__) + '/../lib/retryable'
 require 'rspec'
 
@@ -6,4 +5,12 @@ RSpec.configure do |config|
   # Remove this line if you don't want RSpec's should and should_not
   # methods or matchers
   require 'rspec/expectations'
+
+  def count_retryable(*opts)
+    @try_count = 0
+    return Kernel.retryable(*opts) do |*args|
+      @try_count += 1
+      yield *args
+    end
+  end
 end
