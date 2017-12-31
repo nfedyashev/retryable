0d8e361a6fe3730a3f78b590afedb0196d2b013a
Chubchenko <artem.chubchenko@gmail.com>
2017-12-31 22:48:46 +0200

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'retryable'
+require 'simplecov'
 require 'pry'
 
 Dir.glob(File.expand_path('../support/**/*.rb', __FILE__), &method(:require))
+
+SimpleCov.start
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
